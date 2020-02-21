403d5326a9ed1858da7acbb3c135e29732afdf12
Sergio Sanchez <ronero.canario@gmail.com>
2020-02-21 22:11:55 +0100

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,60 @@
  */
 
 module.exports = {
-  /* Your site config here */
+  siteMetadata: {
+    siteUrl: 'https://www.example.com',
+    title: "Desarrollo Web y Marketing",
+    description: "Nuestro estudio diseña y desarrolla sitios webs rápidos y con un marketing y SEO de alta calidad.",
+    author: "Sergio Sanchez",
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    { 
+      resolve: `gatsby-source-filesystem`,
+      options: {      
+        path: `${__dirname}/src/images/`,
+      } 
+    },
+    {
+      resolve: `gatsby-plugin-sitemap`,
+      options: {
+        // Exclude specific pages or groups of pages using glob parameters
+        // See: https://github.com/isaacs/minimatch
+        // The example below will exclude the single `path/to/page` and all routes beginning with `category`
+        exclude: [`/category/*`, `/path/to/page`],
+        query: `
+          {
+            site {
+              siteMetadata {
+                siteUrl
+              }
+            }
+  
+            allSitePage {
+              edges {
+                node {
+                  path
+                }
+              }
+            }
+        }`,
+        serialize: ({ site, allSitePage }) =>
+          allSitePage.edges.map(edge => {
+            return {
+              url: site.siteMetadata.siteUrl + edge.node.path,
+              changefreq: `daily`,
+              priority: 0.7,
+            }
+          })
+      }
+    },
+    {
+      resolve: 'gatsby-plugin-robots-txt',
+      options: {
+        host: 'https://www.example.com',
+        sitemap: 'https://www.example.com/sitemap.xml',
+        policy: [{ userAgent: '*', allow: '/' }]
+      }
+    },
+  ],
 }
